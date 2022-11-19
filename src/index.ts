00d63f6d8b9c1bec740df21064b33a02b2441744
Issei Mori <isseieikisouya@outlook.jp>
2022-11-19 23:52:05 +0900

    分割代入

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,19 @@
-type Animal = {
-    name:string
+type T = {
+    foo : string;
+    bar : number
 }
 
-type Human = {
-    name:string,
-    age :number
+const obj : T = {
+    foo : 'hello',
+    bar : 123
 }
 
-type Family<Parent = Animal,Child = Animal> = {
-    mother:Parent,
-    father:Parent,
-    child:Child
+const {foo, bar:barVar} = obj;
+
+const nested = {
+    num : 123,
+    obj : {
+        foo : 'hello',
+        bar : 'world'
+    }
 }
-
-type S = Family<string,Human>
-type T = Family
-type U = Family<Human>
-//今んとこ右側だけを指定するとかはできない
-
-type t = number[]
-
-const arr2 : Array<number> = [1]
-
-const arr3 : t = arr2
-
-const arr : readonly number[] = [1,20]
-
-for (const ar of arr) {
-    console.log(ar);
-}
-
-let tuple : [string,number] = ['d',0]
