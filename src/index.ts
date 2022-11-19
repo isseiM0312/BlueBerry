cbcc01cef7a822cca0c7d602607d1caccad9226b
Issei Mori <isseieikisouya@outlook.jp>
2022-11-20 02:40:49 +0900

    モダンJSの記法慣れねえな...

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-type T = {
-    foo : string;
-    bar : number
+type User = {
+    name : string,
+    age : number,
+    premiumUser : boolean
 }
 
-const obj : T = {
-    foo : 'hello',
-    bar : 123
-}
+const data: string = `
+uhyo,26,1
+John Smith,27,0
+Mary Sue,14,1
+`;
 
-const {foo, bar:barVar} = obj;
+const users : User[] = []
 
-const nested = {
-    num : 123,
-    obj : {
-        foo : 'hello',
-        bar : 'world'
+const lines = data.split('\n');
+
+for (const line of lines) {
+    if (line === '') {
+        continue;
     }
+    const [name,ageString,premiumUserString] = line.split(',');
+    const age = Number(ageString);
+    const premiumUser = premiumUserString === '1'
+
+    users.push({
+        name,
+        age,
+        premiumUser
+    });
+}
+
+for (const user of users) {
+    console.log(`${user.name} (${user.age})はプレミアムユーザー${user.premiumUser ? 'です。' : 'ではありません。'}`)
 }
