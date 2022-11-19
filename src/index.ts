9c1bed70b23e47c778a28526e20e4165ef841607
Issei Mori <isseieikisouya@outlook.jp>
2022-11-19 20:33:02 +0900

    typeof

--- src/index.ts
+++ src/index.ts
@@ -10,3 +10,39 @@ const obj : FooBarObj = {
 
 type tt = string;
 const t : tt = 'dd';
+
+type aobj = {
+    [key:string] : number
+}
+
+const data : aobj = {
+    'da':14
+}
+
+type optobj = {
+    a : string,
+    b? : string
+}
+
+const ins : optobj = {
+    a : 'd'
+}
+
+type freezed = {
+    readonly foo : string
+}
+
+try {
+    let freeze : freezed = {
+        foo : 'dfa'
+    }
+} catch (e) {
+    console.log(e)
+}
+
+const obj3 = {
+    foo: 'apiとかから型のわからないデータが返ってきた時のことを考えろ',
+    bar: 'anyは使いたくないし、最初の一回とかだったら撮ってきたデータの型に応じて変更不可なかたを定義したい時あるよね',
+    hoge: 'そんな時typeofを使うんだ、typeみたいに明示的に型を決めてあげなくても、型のコピーができる、apiからのデータがある時numberでまたある時stringなら、実行するごとに',
+    fuga: '型を変えて定義してくれてかつちゃんとコンパイルエラーも出すよ'
+}
