d905a49956ff373702e5576d08232af9ee57a5fb
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 05:29:09 +0900

    インターセクション型

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,34 @@
 type Animal = {
     species : string;
-    age: string;
+    age: number;
 }
 
-type Human = {
+type Human = Animal & {
     name : string;
-    age : number;
 }
 
 type User = Animal | Human
 
 const tama : User = {
     species : 'cat',
-    age : 'fuck'
+    age : 6
 }
 
 const uhyo : User = {
+    species : '',
     name : 'uhyo',
-    age:26
+    age:26,
 }
 
 function showAge(user: User) {
-    const age = user.age; //ageの型はunionの伝播によりstring | numberになる
+    const age = user.age; 
     console.log(age);
 }
+
+//インターセクション型で作った方は大体部分型になる（より厳しく情報を求められるから)
+
+type crazy = number & string
+//異なるプリミティブ同士でやるとnevernになる。
+
+type lesscrazy = Animal & string
+//オブジェクトとプリミティブは、プリミティブがオブジェクトっぽい動き(例えばstring.lengh)とかするかもなので即neverではない。
