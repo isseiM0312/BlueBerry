bc956454083696ce7e3590e86b8c558d45710b45
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 05:23:32 +0900

    ユニオン型の伝播

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 type Animal = {
-    species : string
+    species : string;
+    age: string;
 }
 
 type Human = {
-    name : string
+    name : string;
+    age : number;
 }
 
 type User = Animal | Human
 
 const tama : User = {
-    species : 'cat'
+    species : 'cat',
+    age : 'fuck'
 }
 
 const uhyo : User = {
-    name : 'uhyo'
+    name : 'uhyo',
+    age:26
+}
+
+function showAge(user: User) {
+    const age = user.age; //ageの型はunionの伝播によりstring | numberになる
+    console.log(age);
 }
