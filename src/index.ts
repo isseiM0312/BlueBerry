fd1e46cd7503058f42a7bf7e45ab90abd2b4e7c7
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 04:52:16 +0900

    大域脱出とエラー検知

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,15 @@
-//　obj.methodの時 this.somethingを中で読んでたらthisはobjを指す、
-//　上の形じゃないとthisはundefinedになる
-// 例えばobj.methodを関数として代入して代入した関数を呼び出すとthis効いてないからエラーになる
-
-class User {
-    name : string;
-    #age: number;
-
-    constructor(name:string,age:number) {
-        this.name = name;
-        this.#age = age;
-    }
-
-    public isAdult(): boolean {
-        return this.#age >= 20;
-    }
-
-    public filterOlder(users:readonly User[]): User[] {
-        return users.filter(u => u.#age > this.#age)
-    }
+try {
+    throwError();
+    console.log('ここも表示されない');
+} catch (err) {
+    console.log(err);
 }
 
-const uhyo = new User('uhyo',25);
-const john = new User('Jhon',15);
-const bob = new User('Bob',40);
+function throwError() {
+    const error = new Error('エラー');
+    throw error;
+    console.log('こいつも表示されない')
+}
 
-const older = uhyo.filterOlder([john,bob])
-
-//ただしアロー関数のthisはどう呼び出したかは関係ない
+//帯域脱出によってtryがないと永遠と外へ逃げていき限界までいくとクラッシュする。
+//これによってネストしてた処理をのエラーを一箇所で拾える。
