bfb027e9d898669bd189377557e2e2ccb290f033
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 02:07:35 +0900

    クラス

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-function map<T,U>(array: T[], callback: (i : T) =>  U) : U[] {
- const result : U[] = [];
- for (const elm of array) {
-    result.push(callback(elm));
- }
- return result;
+class User {
+    name : string = '';
+    age : number = 0;
+
+    isAdult(): boolean {
+        return this.age > 10;
+    }
+
+    setAge(newAge: number) {
+        this.age = newAge;
+    }
 }
 
-const data = [1,2,3,4,5,6,7,8,9]
+const uhyo = new User();
 
-const result = map(data,(x) => x * 10);
-
-for (const elm of result) {
-    console.log(elm);
-}
+console.log(uhyo.isAdult());
+uhyo.setAge(26);
+console.log(uhyo.isAdult());
