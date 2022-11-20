f91e1e3ba49cd4c99120be32b74c13927e5cfbbb
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 04:16:34 +0900

    protected

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,21 @@
 class User {
     name:string;
-    #age:number;
+    protected age:number;
 
     constructor(name:string,age:number) {
         this.name = name;
-        this.#age = age;
+        this.age = age;
     }
 
     public isAdult(): boolean {
-        return this.#age > 20;
+        return this.age > 20;
     }
 
 }
 
 class PremiumUser extends User {
-    rank : number = 1;
 
-    public override isAdult(): boolean {
-        return true;
+    public isAdult(): boolean {
+        return this.age >= 10;
     }
 }
-
-//superは結局親のコンストラクタを呼ぶやつ。上ではnameとageを親のコンストラクタ呼び出して定義してる。
