5fc8ad82beaab6303e642c7f4bd7c2597ccf811f
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 03:26:26 +0900

    クラスの型

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
-class User {
+class User<T> {
     name:string;
     #age:number;
+    readonly data: T;
 
-    constructor(name:string, age:number) {
+    constructor(name: string, age:number, data:T) {
         this.name = name;
         this.#age = age;
+        this.data = data;
     }
 
     public isAdult(): boolean {
-        return this.#age > 20;
+        return this.#age >= 20;
     }
+
 }
+
+const join = new User<{num:number}>('John',15,{num:14})
