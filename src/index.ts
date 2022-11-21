129c87e3bcb5a0cb9f2e75b7c29e41793e223c4a
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 16:27:16 +0900

    lookup型

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,26 @@
-type Animal = {
-    tag:'animal';
-    species: string;
-}
-
 type Human = {
-    tag:'human';
-    name: string;
+    type : 'human';
+    name : string;
+    age : number;
 }
 
-type User = Animal | Human;
+//T<S>みたいに型を渡せるのがlookup型
 
-
-/* const alien : User = {
-    tag : 'alien',
-    name : 'www'
-} */
-
-//これはコンパイルエラー
-
-function getUserName(user : User) {
-    if (user.tag === 'human') {
-        return user.name
-    } else {
-        return '名無し'
-    } 
+function setAge(human: Human, age : Human['age']) {
+    return(
+        {
+            ...human,
+            age
+        }
+    )
 }
 
-const tama : User = {
-    tag : 'animal',
-    species : 'dog'
+const uhyo: Human = {
+    type:'human',
+    name:'uhyo',
+    age:26
 }
 
-const uhyo : User = {
-    tag : 'human',
-    name : 'uhyo'
-}
+const uhyo2 = setAge(uhyo,27)
 
-
-console.log(getUserName(tama));
-console.log(getUserName(uhyo));
-
-//このようにすることで、扱われうるすべてのデータを含める型を定義できて、
-//扱う側はtagで絞り込みを行って型確定させてから使える
-
-//これが擬似的代数的データ型の威力だ！
+//lookup型で型を再利用できる！
