833cc0b74d2c2f4d8a2c128952b460819212674e
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 18:31:23 +0900

    conditional type

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-type Fruit = 'apple' | 'orange' | 'strawberry';
+//型の条件分岐
+//ジェネリクスで主に使われる
 
-type FruitArrays = {
-    [P in Fruit]: P[]
+type RestArgs<M> = M extends 'string' ? [string,string] : [number,number,number]
+
+function func<M extends 'string' | 'number'>(mode: M, ...args: RestArgs<M>) {
+
 }
 
-const numbers: FruitArrays = {
-    apple:['apple'],
-    orange:['orange','orange'],
-    strawberry:[]
-}
+//funcにMとしてstringかnumberのやつを入れると、stringがあるなら[string,string]型になるってこと。
 
+func('string','uhyo','hyo');
+
+//union distributionってのもある
+//Mがユニオン型だとド・モアブルみたいになる
