dbff6ba44e42937ce29199db72a899d6c5d3d9d5
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 15:29:54 +0900

    擬似的代数的データ型

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,46 @@
-console.log(typeof 'uhyo');
-console.log(typeof 25);
-console.log(typeof {});
-console.log(typeof undefined);
+type Animal = {
+    tag:'animal';
+    species: string;
+}
 
-//typeofは文字列を返す
+type Human = {
+    tag:'human';
+    name: string;
+}
+
+type User = Animal | Human;
+
+
+/* const alien : User = {
+    tag : 'alien',
+    name : 'www'
+} */
+
+//これはコンパイルエラー
+
+function getUserName(user : User) {
+    if (user.tag === 'human') {
+        return user.name
+    } else {
+        return '名無し'
+    } 
+}
+
+const tama : User = {
+    tag : 'animal',
+    species : 'dog'
+}
+
+const uhyo : User = {
+    tag : 'human',
+    name : 'uhyo'
+}
+
+
+console.log(getUserName(tama));
+console.log(getUserName(uhyo));
+
+//このようにすることで、扱われうるすべてのデータを含める型を定義できて、
+//扱う側はtagで絞り込みを行って型確定させてから使える
+
+//これが擬似的代数的データ型の威力だ！
