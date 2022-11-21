887073190c0ad26994b1df39a0bb9073d394671e
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 15:12:53 +0900

    typeofを使った型の絞り込み

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,6 @@
-type SignType = 'plus' | 'minus';
-function signNumber(type : SignType) {
-    return type === 'plus' ? 1 : -1;
-}
+console.log(typeof 'uhyo');
+console.log(typeof 25);
+console.log(typeof {});
+console.log(typeof undefined);
 
-function numberWithSign(num: number, type : SignType | 'none') {
-    if (type === 'none') {
-        return 0;
-        //ここではnone型
-    } else {
-        return num *signNumber(type);
-        //ここではSignType型
-    }
-}
-
-console.log(numberWithSign(5,'plus'))
-console.log(numberWithSign(5,'minus'))
-console.log(numberWithSign(5,'none'))
-
-//コードで明示的にその型になるしかないように絞られてる(例えばIf文などで)ときは、その絞られた方で処理を続ける
+//typeofは文字列を返す
