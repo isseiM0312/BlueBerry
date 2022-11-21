0de5606639fcb8ef3377668ac468b5d01038ae99
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 14:54:01 +0900

    オブジェクトのプロパティもwinding

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,8 @@ const uhyo1 = 'uhyo';
 
 let uhyo2 = 'uhyo';
 //let uhyo2: string 再代入が期待されるため、stringに拡張される、これをwindingという
+
+const uhyo3 = {
+    name : 'uhyo', //(property) name: string オブジェクト型はconstでも中身変えられるのでwindingが起こる
+    age : 26
+}
