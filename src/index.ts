627f4374ad993988360de11e027273d5f077939e
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 18:04:14 +0900

    is

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,7 @@
-type HasToString = {
-    toString: () => string;
-}
-//HasToStringの型制限は、toStringっていうstring型を返すメソッドを持ってるオブジェクトってだけで、
-//実はnumberとかのprimitive型もtoString持ってる
-
-function useToString1(value: HasToString & object) {
-    console.log(`value is ${value.toString()}`)
+function isStringOnNumber(value: unknown) : value is string | number {
+    return typeof value === 'string' || typeof value === 'number';
 }
 
-useToString1({
-    toString() {
-        return 'fsg'
-    }
-})
+//引数名 is 型　ってのを型制限に書いて、返り値がtrueならisの後ろに書いてある型を返す。
 
-//useToString1(2)
-//&objectつけるとエラー出る
-
-//never型は空集合みたいなやつ
-//何にでも代入できるか、値がないのでnever型のやつには何も代入できない
-function userNever(value: never) {
-    const num = value;
-}
-
-//userNever({})
-
-//neverは全ての型の部分型である！(最も条件が厳しいからね！)
+//assertはvoidで、例外検知で型を判定する時に使える
