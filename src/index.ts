4e7534f0ff07a2df7bfb2c55e81f0c4b09d251f9
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 14:48:25 +0900

    ユニオン型とリテラル型の併用

--- src/index.ts
+++ src/index.ts
@@ -21,3 +21,10 @@ function getHelloStr() : `Hello ${string}!` {
         //return 'Hello Worldq'
     }
 }
+
+function signNumber(type  : 'plus' | 'minus') {
+    return type === 'plus' ? 1 : -1;
+}
+
+console.log(signNumber('plus'));
+console.log(signNumber('minus'));
