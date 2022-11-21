8dc7a380887c5f426b208113b7bc5c4e2e4327fd
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 15:10:46 +0900

    型の絞り込み

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
-type uhyo = 'uhyo';
-
-const uhyo1 = 'uhyo';
-
-let uhyo2 = 'uhyo';
-//let uhyo2: string 再代入が期待されるため、stringに拡張される、これをwindingという
-
-const uhyo3 = {
-    name : 'uhyo', //(property) name: string オブジェクト型はconstでも中身変えられるのでwindingが起こる
-    age : 26
+type SignType = 'plus' | 'minus';
+function signNumber(type : SignType) {
+    return type === 'plus' ? 1 : -1;
 }
-//windingは文脈の効果を受ける
 
-let uhyo4: 'uhyo' = 'uhyo';
-//プログラマが明確に宣言すればwindingされない
+function numberWithSign(num: number, type : SignType | 'none') {
+    if (type === 'none') {
+        return 0;
+        //ここではnone型
+    } else {
+        return num *signNumber(type);
+        //ここではSignType型
+    }
+}
+
+console.log(numberWithSign(5,'plus'))
+console.log(numberWithSign(5,'minus'))
+console.log(numberWithSign(5,'none'))
+
+//コードで明示的にその型になるしかないように絞られてる(例えばIf文などで)ときは、その絞られた方で処理を続ける
