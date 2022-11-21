2b131138839ed9eaa7e420e8e7868c8af4bd8b3b
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 14:34:50 +0900

    テンプレートリテラル型

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,13 @@ type t = true;
 type three = 3n;
 
 //リテラル型は4種類ある
+
+function getHelloStr() : `Hello ${string}!` {
+    const rand = Math.random();
+    if (rand < 0.3) {
+        return 'Hello World!';
+    } else {
+        return 'Hello World!'
+        //return 'Hello Worldq'
+    }
+}
