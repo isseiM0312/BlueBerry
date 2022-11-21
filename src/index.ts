daad9c251291e79dead0ddb4f3fcefcd41f06643
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 17:29:00 +0900

    as const は仲間！

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
-//as はTSでは基本的に使うべきじゃない！
-//typeof は typeof 変数　=== 型　じゃないと型の絞り込みを自動で行なってくれない。
-//上以外のタイミングで人間には明らかに型決まってるやん！て時に
-//TSの型予測システムを覆して処理書く時に使う
+//as const は味方！
 
-type Animal  = {
-    tag : 'animal',
-    species : 'string' 
-}
+const name1 = ['uhyo','John','Taro'];
+//const name1: string[]
 
-type Human = {
-    tag : 'human',
-    name : string
-}
+const name2 = ['uhyo','John','Taro'] as const;
+//const name2: readonly ["uhyo", "John", "Taro"]
 
-type User = Animal | Human;
-
-function getNamesIfAllHuman(users:readonly User[]): string[] | undefined {
-    if (users.every(user => user.tag === 'human')) {
-        return (users as Human[]).map(user => user.name);
-    }
-    return undefined;
-}
+//データから型きめる形！
+type Names = (typeof name2)[number];
