8c11d727faab26ad5e9a04a67cd5c6bf1c886f96
Issei Mori <isseieikisouya@outlook.jp>
2022-11-21 18:07:31 +0900

    可変長タプル型

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-function isStringOnNumber(value: unknown) : value is string | number {
-    return typeof value === 'string' || typeof value === 'number';
-}
+type NumberAndStrings = [number,...string[]]
 
-//引数名 is 型　ってのを型制限に書いて、返り値がtrueならisの後ろに書いてある型を返す。
-
-//assertはvoidで、例外検知で型を判定する時に使える
+type NumberStringNumber = [number,...string[],number]
